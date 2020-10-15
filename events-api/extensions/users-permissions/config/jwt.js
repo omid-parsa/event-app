df31f49685068c0bbc2f978bfe81e866b60b3672
Omid Parsa <omidparsa.1985@gmail.com>
2020-10-16 00:46:28 +0200

    first commit

---
+++ events-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b7bef36-cd6a-4ed4-841e-c8c249ea91c7'
+};
---
+++ events-front/src/templates/Event.vue
@@ -0,0 +1,30 @@
+<template>
+    <Layout>
+        <h1>{{$page.event.title}}</h1>
+        <img :src="`${$page.event.imageUrl}`"/>
+        <p>{{$page.event.description}}</p>
+    </Layout>
+</template>
+
+<page-query>
+query {
+  event(id: "4") {
+    id
+    title
+    description
+    price
+    date
+    imageUrl
+  }
+}
+</page-query>
+
+<script>
+export default {
+    metaInfo() {
+        return {
+            title: this.$page.event.title
+        }
+    }
+}
+</script>
